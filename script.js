d1e34139bb589f7cc7ead56a112bcaa706cbf150
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-11 21:56:24 -0800

    added numbers array

--- script.js
+++ script.js
@@ -38,4 +38,11 @@ document.getElementById('generate').addEventListener('click', () => {
     }
   }
 
+  if (number) {
+    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for (let i = 0; i < numbers.length; i++) {
+      choices.push(numbers[i])
+    }
+  }
+
 })
