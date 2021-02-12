f27196f37d9680b06d5f19bf5698bd4408870e27
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-11 21:55:20 -0800

    added uppercase array

--- script.js
+++ script.js
@@ -31,4 +31,11 @@ document.getElementById('generate').addEventListener('click', () => {
     }
   }
 
+  if (upper) {
+    let uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    for (let i = 0; i < uppers.length; i++) {
+      choices.push(uppers[i])
+    }
+  }
+
 })
