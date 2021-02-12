e5ffe5ddfd7750837aa2c51aa2b791a2b0c4b5c6
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-12 10:23:10 -0800

    changed arrays to string to clean code

--- script.js
+++ script.js
@@ -21,34 +21,22 @@ document.getElementById('generate').addEventListener('click', () => {
     special = confirm('Do you want to include special characters? (OK for yes, Cancel for no)')
   }
 
-  let choices = []
+  let choices = ''
 
   if (lower) {
-    let lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    for (let i = 0; i < lowers.length; i++) {
-      choices.push(lowers[i])
-    }
+    choices += 'abcdefghijklmnopqrstuvwxyz'
   }
 
   if (upper) {
-    let uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    for (let i = 0; i < uppers.length; i++) {
-      choices.push(uppers[i])
-    }
+    choices += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
 
   if (number) {
-    let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for (let i = 0; i < numbers.length; i++) {
-      choices.push(numbers[i])
-    }
+    choices += '1234567890'
   }
 
   if (special) {
-    let specials = ['\ ', '\!', '\"', '\#', '\$', '\%', '\&', '\'', '\(', '\)', '\*', '\+', '\,', '\-', '\.', '\/', '\:', '\;', '\<', '\=', '\>', '\?', '\@', '\[', '\\', '\]', '\^', '\_', '\`', '\{', '\|', '\}', '\~']
-    for (let i = 0; i < specials.length; i++) {
-      choices.push(specials[i])
-    }
+    choices += '\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~'
   }
 
   let password = '';
