beb9ede6aad10574a8110b54dd9512bf70ec4aa5
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-11 21:54:33 -0800

    added choice array and array for lowercase letters

--- script.js
+++ script.js
@@ -22,4 +22,13 @@ document.getElementById('generate').addEventListener('click', () => {
     console.log(special)
   }
 
+  let choices = []
+
+  if (lower) {
+    let lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for (let i = 0; i < lowers.length; i++) {
+      choices.push(lowers[i])
+    }
+  }
+
 })
