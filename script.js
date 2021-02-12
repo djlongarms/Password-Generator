360cf88f2b5f707e5ab29f05d2640edb68f72632
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-12 09:59:18 -0800

    added check to ensure length is a number

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ document.getElementById('generate').addEventListener('click', () => {
 
   while (length < 8 || length > 128) {
     length = parseInt(prompt('How long would you like the password to be? (Must be between 8 and 128 characters)'), 10)
+    if(isNaN(length)) {
+      length = 0;
+    }
   }
 
   let lower = false
